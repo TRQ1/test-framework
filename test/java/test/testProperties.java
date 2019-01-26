4abe5597e06643ee14265c66465874b0e0d2211b
이규원 <trq1.lee@gmail.com>
2019-01-26 15:46:53 +0900

    Properties 수정

--- test/java/test/testProperties.java
+++ test/java/test/testProperties.java
@@ -1,25 +1,53 @@
 package test;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class testProperties {
-    public static void main(String[] args) {
-        try {
-            String filePath ="D:\\Projects\\test-framework\\test\\resource\\config.properteis";
-            Properties properties = new Properties();
-            FileInputStream fs = new FileInputStream(filePath);
-            properties.load(new BufferedInputStream(fs));
 
-            System.out.println("user.id: " + properties.getProperty("user.id"));
-            System.out.println("user.name: " + properties.getProperty("user.name"));
-            System.out.println("company.name: " + properties.getProperty("company.name"));
-            System.out.println("company.address: " + properties.getProperty("company.address"));
+    private Properties properties;
 
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+    public testProperties() {
+        properties = new Properties();
     }
+
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    /**
+     * Properties 파일을 Inputstream으로 Load하여 맴버 변수의 Properties에 저장한다.
+     *
+     * @param path
+     * @throws IOException
+     */
+    public void loadProperties(String path) throws IOException {
+        InputStream propPath = getClass().getClassLoader().getResourceAsStream(path);
+        properties.load(propPath);
+        propPath.close();
+    }
+
+    public static Properties loadPropertiesStatic(String path) throws IOException {
+        Properties properties = new Properties();
+        InputStream inputStream = testProperties.class.getClassLoader().getResourceAsStream(path);
+        properties.load(inputStream);
+        inputStream.close();
+        return properties;
+    }
+
+    public static void main(String[] args) throws IOException {
+        testProperties testProperties = new testProperties();
+        testProperties.loadProperties("/config.properteis");
+
+        Properties properties = testProperties.getProperties();
+
+        System.out.println("user.id: " + properties.getProperty("user.id"));
+        System.out.println("user.name: " + properties.getProperty("user.name"));
+        System.out.println("company.name: " + properties.getProperty("company.name"));
+        System.out.println("company.address: " + properties.getProperty("company.address"));
+
+    }
+
 }
